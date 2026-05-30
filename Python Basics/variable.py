0c858ac21e91f507d6ec031bd9109b10d8178e46
govindkrcrimfo <govind.krcrimfo555@gmail.com>
2026-05-30 14:31:55 +0530

    Variables and its decalaration

---
+++ Python Basics/variable.py
@@ -0,0 +1,20 @@
+# Type exists ✅
+# Type declaration is optional ✅
+# Type checking happens mostly at runtime ✅
+
+a=10            # integer 
+print(a)
+a="Govind"      #string
+print(a)
+a=2+3j          #complex number
+print(a)
+
+print()
+
+# Type declaration is optional ✅
+b:int=10
+print(b)
+b:str="Govind"
+print(b)
+b:complex=2 + 3j
+print(b)
