0bd0f5fafd0a1bcf27bf11984240f9919bbabc6a
govindkrcrimfo <govind.krcrimfo555@gmail.com>
2026-05-30 19:00:32 +0530

    String and its inbuilt function

---
+++ Python Basics/string&itsFunctions.py
@@ -0,0 +1,17 @@
+s="Hello Govind !!"
+print(s)
+print(len(s))
+print(s.upper())
+print(s.lower())
+print(s[0])
+
+#Slicing -> s[i:j] -> i include , j excluded
+print(s[1:5])
+a="kumaru"
+print(s+a) #concatination
+
+print(a.find("u"))      # first index
+
+print(a.replace('m','n'))   #replace m to n
+
+print(s.split())        #split based on spaces 
